6b9c0ef101a2abc85ec8ce78657c5dfa30c54dcf
Zak <zak@offworldsim.com>
2020-02-05 10:16:27 -0800

    Setup windows build script, and setup VSCode so it can run the build script

---
+++ source/build.bat
@@ -0,0 +1,16 @@
+@echo off
+
+REM NOTE(zak): This is an attempt to find cl if the user doesn't have CL. 
+where /q cl
+if ERRORLEVEL 1 (
+    for /f "delims=" %%a in ('"%ProgramFiles(x86)%\Microsoft Visual Studio\Installer\vswhere.exe" -find VC\Auxiliary\Build\vcvarsall.bat') do (%%a x64)
+)
+
+
+IF NOT EXIST ..\build mkdir ..\build
+
+pushd ..\build
+
+cl -nologo -FC -Zi ..\source\win32_tinyengine.c
+
+popd
---
+++ source/win32_tinyengine.c
@@ -0,0 +1,8 @@
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+int WINAPI
+WinMain(HINSTANCE Instance, HINSTANCE PrevInstance, LPSTR CommandLine, int ShowCode)
+{
+    return(0);
+}
