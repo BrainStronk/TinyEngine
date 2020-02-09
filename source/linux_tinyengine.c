ff15d8049276df0c1360e6362562233d8b7813fc
Kyryl Melekhin <k.melekhin@gmail.com>
2020-02-09 08:43:24 -0500

    Proper style & events

--- source/linux_tinyengine.c
+++ source/linux_tinyengine.c
@@ -6,39 +6,112 @@
 #include <string.h>
 
 #include "tinyengine_types.h"
- 
+
+/*
+To avoid naming conflicts and ability to create more than 1 window -
+this is a struct.
+*/
+typedef struct linux_wnd
+{ 
+	Display* Display;
+	Window Window;
+	XSetWindowAttributes Attr;
+	s32 Screen;
+	u32 Width;
+	u32 Height; 
+	u32 Depth;
+	u32 ValueMask;
+} linux_wnd;
+
+static linux_wnd Wnd;
+
 int main(int argc, char** argv)
 {
-   Display *d;
-   Window w;
-   XEvent e;
-   const char *msg = "Hello, World!";
-   s32 s;
- 
-   d = XOpenDisplay(NULL);
-   if (d == NULL)
+   Wnd.Display = XOpenDisplay(getenv("DISPLAY"));
+   if (Wnd.Display == NULL)
    {
       fprintf(stderr, "Cannot open display\n");
       exit(1);
    }
  
-   s = DefaultScreen(d);
-   w = XCreateSimpleWindow(d, RootWindow(d, s), 10, 10, 100, 100, 1,
-                           BlackPixel(d, s), WhitePixel(d, s));
-   XSelectInput(d, w, ExposureMask | KeyPressMask);
-   XMapWindow(d, w);
+   Wnd.Screen = XDefaultScreen(Wnd.Display);
+
+   Wnd.Attr.background_pixel = XBlackPixel(Wnd.Display, Wnd.Screen);
+   Wnd.ValueMask |= CWBackPixel;
+   Wnd.Depth = DefaultDepth(Wnd.Display, Wnd.Screen);
+   /*
+      TODO(Kyryl): Wnd.Width && Wnd.Height will be the max extent from vulkan swapchain
+      vulkan initialization is performed after window creation to create a vulkan surface.
+    */
+   Wnd.Width = 640;
+   Wnd.Height = 640;
+   Wnd.Window = XCreateWindow(Wnd.Display, XRootWindow(Wnd.Display, Wnd.Screen),
+		   0, 0, Wnd.Width, Wnd.Height,
+		   0, Wnd.Depth, InputOutput,
+		   DefaultVisual(Wnd.Display, Wnd.Screen),
+		   Wnd.ValueMask, &Wnd.Attr);
+
+
+   XSelectInput(Wnd.Display, Wnd.Window, ExposureMask | KeyPressMask);
+   XMapWindow(Wnd.Display, Wnd.Window);
  
+   XEvent Event;
    while (1) 
    {
-      XNextEvent(d, &e);
-      if (e.type == Expose)
-      {
-         XFillRectangle(d, w, DefaultGC(d, s), 20, 20, 10, 10);
-         XDrawString(d, w, DefaultGC(d, s), 10, 50, msg, strlen(msg));
-      }
+	   //see https://tronche.com/gui/x/xlib/events/structures.html 
+	   XNextEvent(Wnd.Display, &Event);
+	   switch(Event.type)
+	   {
+		   case KeyPress:
+			   break;
+		   case KeyRelease:
+			   break;
+		   case ButtonPress:
+			   break;
+		   case ButtonRelease:
+			   break;
+		   case MotionNotify:
+			   break;
+		   case EnterNotify:
+			   break;
+		   case LeaveNotify:
+			   break;
+		   case FocusIn:
+			   break;
+		   case FocusOut:
+			   break;
+		   case KeymapNotify:
+			   break;
+		   case Expose:
+			   break;
+		   case GraphicsExpose:
+			   break;
+		   case NoExpose:
+			   break;
+		   case VisibilityNotify:
+			   break;
+		   case CreateNotify:
+			   break;
+		   case DestroyNotify:
+			   break;
+		   case UnmapNotify:
+			   break;
+		   case MapNotify:
+			   break;
+		   case MapRequest:
+			   break;
+		   case ReparentNotify:
+			   break;
+		   case ConfigureNotify:
+			   break;
+		   case ConfigureRequest:
+			   break;
+		   case GravityNotify:
+			   break;
+	   }
    }
  
-   XCloseDisplay(d);
+   XCloseDisplay(Wnd.Display);
    return 0;
 }
 
